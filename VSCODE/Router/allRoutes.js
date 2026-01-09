3191d16f18c9d7ff1a92f520d4294da17b31442d
Nom d'utilisateur GitHubMilady28002 <sylvie.gfm@gmail.com>
2026-01-09 14:36:25 +0100

    feat: ajustement typographies et mise en page accueil

--- VSCODE/Router/allRoutes.js
+++ VSCODE/Router/allRoutes.js
@@ -1,7 +1,8 @@
 import Route from "./Route.js";
 
 export const allRoutes = [
-  new Route("/", "Accueil", "./pages/home.html")
+  new Route("/", "Accueil", "./pages/home.html"),
+  new Route("/decouvrir", "Découvrir", "./pages/decouvrir.html"),
 ];
 
 export const websiteName = "Vite & Gourmand";
