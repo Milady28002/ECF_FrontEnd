f8b4635b82d49f632ca69d1b7f7d7a3ceb4075f2
Nom d'utilisateur GitHubMilady28002 <sylvie.gfm@gmail.com>
2026-01-15 10:25:49 +0100

    Modifs html home et footer

--- VSCODE/Router/allRoutes.js
+++ VSCODE/Router/allRoutes.js
@@ -3,6 +3,8 @@ import Route from "./Route.js";
 export const allRoutes = [
   new Route("/", "Accueil", "./pages/home.html"),
   new Route("/formules", "Nos formules", "./pages/formules.html"),
+  new Route("/galerie", "La galerie", "./pages/galerie.html"),
+  new Route("/contact", "Nous contacter", "./pages/contact.html"),
   ];
 
 export const websiteName = "Vite & Gourmand";
