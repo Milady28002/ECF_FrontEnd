e38f597ad503f757c871056519cc26352efebfa1
Nom d'utilisateur GitHubMilady28002 <sylvie.gfm@gmail.com>
2026-01-20 09:27:53 +0100

    Création pages signin et signup

--- VSCODE/Router/allRoutes.js
+++ VSCODE/Router/allRoutes.js
@@ -6,6 +6,8 @@ export const allRoutes = [
   new Route("/galerie", "La galerie", "./pages/galerie.html"),
   new Route("/contact", "Nous contacter", "./pages/contact.html"),
   new Route("/menus", "Nos menus", "./pages/menus.html"), 
+  new Route("/signin", "Se connecter", "./pages/signin.html"), 
+  new Route("/signup", "S'inscrire", "./pages/signup.html"),
   ];
 
 export const websiteName = "Vite & Gourmand";
