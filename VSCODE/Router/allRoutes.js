c9c4d04167d08d7af29a06bc68b5f57d14d18f94
Nom d'utilisateur GitHubMilady28002 <sylvie.gfm@gmail.com>
2026-01-22 16:31:48 +0100

    Création pages classique et commandes + ajustements

--- VSCODE/Router/allRoutes.js
+++ VSCODE/Router/allRoutes.js
@@ -2,13 +2,14 @@ import Route from "./Route.js";
 
 export const allRoutes = [
   new Route("/", "Accueil", "./pages/home.html"),
-  new Route("/formules", "Nos formules", "./pages/formules.html"),
+  new Route("/menus", "Nos Menus", "./pages/nos-menus/menus.html"),
   new Route("/galerie", "La galerie", "./pages/galerie.html"),
   new Route("/contact", "Nous contacter", "./pages/contact.html"),
-  new Route("/menus", "Nos menus", "./pages/menus.html"), 
   new Route("/signin", "Se connecter", "./pages/auth/signin.html"), 
   new Route("/signup", "S'inscrire", "./pages/auth/signup.html"),
+  new Route("/commandes", "Mes commandes", "./pages/commandes.html"),
   new Route("/modifPassword","Mot de passe oublié","./pages/auth/modifPassword.html"),
+  new Route("/classique", "Classique", "./pages/nos-menus/classique.html")
   ];
 
 export const websiteName = "Vite & Gourmand";
