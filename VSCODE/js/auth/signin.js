d3eb18c46e46b9b15a7b618e7e33132933585619
Nom d'utilisateur GitHubMilady28002 <sylvie.gfm@gmail.com>
2026-01-26 11:57:26 +0100

    Création fichier script.js + méthode de verifs champs

--- VSCODE/js/auth/signin.js
+++ VSCODE/js/auth/signin.js
@@ -7,17 +7,17 @@ btnSignin.addEventListener("click", checkCredentials);
 function checkCredentials() {
     //ici il faudra appeler l'API pour vérifier les credentials en BDD
     if(mailInput.value == "test@mail.com" && PasswordInput.value == "123") {
-        alert("Vous êtes connecté");
 
         //Il faudra récupérer le vrai token
         const token = "141278";
-
+        setToken(token);
         //placer ce token en cookie
 
-        window.location.replace("/");
+        window.location.replace("/"); //redirection vers la page d'accueil
     }
     else{
         mailInput.classList.add("is-invalid");
         PasswordInput.classList.add("is-invalid");
     }
 }
+
