7e8c56ba99c7da76556fba612447bc41d206ed1b
Nom d'utilisateur GitHubMilady28002 <sylvie.gfm@gmail.com>
2026-01-29 14:59:05 +0100

    Harmonisation URL front et back 127.0.0.1

--- VSCODE/js/auth/signin.js
+++ VSCODE/js/auth/signin.js
@@ -1,24 +1,49 @@
 const mailInput = document.getElementById("EmailInput");
 const PasswordInput = document.getElementById("PasswordInput");
 const btnSignin = document.getElementById("btnSignin");
+const signinForm = document.getElementById("signinForm");
 
 btnSignin.addEventListener("click", checkCredentials);
 
-function checkCredentials() {
-    //ici il faudra appeler l'API pour vérifier les credentials en BDD
-    if(mailInput.value == "test@mail.com" && PasswordInput.value == "123") {
+function checkCredentials(){
+    let dataForm = new FormData(signinForm);
 
-        //Il faudra récupérer le vrai token
-        const token = "141278";
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let raw = JSON.stringify({
+        "username": dataForm.get("email"),
+        "password": dataForm.get("mdp"),
+    });
+
+    let requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+fetch(apiUrl+"login", requestOptions)
+  .then(response => {
+        if(response.ok){
+            return response.json();
+        }
+        else{
+            mailInput.classList.add("is-invalid");
+            PasswordInput.classList.add("is-invalid");
+        }
+  })
+  .then(result => {
+        const token = result.apiToken; //libellé sur ma doc API
         setToken(token);
         //placer ce token en cookie
-        setCookie(RoleCookieName, "admin", 7);
+        setCookie(RoleCookieName, result.roles[0], 7); //sur mon API
         
         window.location.replace("/"); //redirection vers la page d'accueil
-    }
-    else{
-        mailInput.classList.add("is-invalid");
-        PasswordInput.classList.add("is-invalid");
-    }
+  })
+
+  .catch(error => console.log('error'.error));
 }
+
+
 
--- VSCODE/js/auth/signup.js
+++ VSCODE/js/auth/signup.js
@@ -5,6 +5,7 @@ const inputMail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
 const inputValidationPassword = document.getElementById("ValidatePasswordInput");
 const btnValidation = document.getElementById("btn-validation-inscription");
+const formInscrition = document.getElementById("formulaireInscrition");
 
 inputNom.addEventListener("keyup", validateForm); 
 inputPrenom.addEventListener("keyup", validateForm);
@@ -12,6 +13,9 @@ inputTelephone.addEventListener("keyup", validateForm);
 inputMail.addEventListener("keyup", validateForm);
 inputPassword.addEventListener("keyup", validateForm);
 inputValidationPassword.addEventListener("keyup", validateForm);
+
+btnValidation.addEventListener("click", InscrireUtilisateur);
+
 
 //Function permettant de valider tout le formulaire
 function validateForm(){
@@ -101,4 +105,43 @@ function validateRequired(input){
         input.classList.add("is-invalid");
         return false;
     }
+}
+
+//fetch
+function InscrireUtilisateur() {
+    let dataForm = new FormData(formInscrition);
+
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let raw = JSON.stringify({
+        "firstName": dataForm.get("nom"),
+        "lastName": dataForm.get("prenom"),
+        "telephone" : dataForm.get("telephone"),
+        "email": dataForm.get("email"),
+        "password": dataForm.get("mdp"),
+    });
+
+    let requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+fetch(apiUrl+"registration", requestOptions)
+  .then(response => {
+        if(response.ok){
+            return response.json();
+        }
+        else{
+            alert("Erreur lors de l'inscription");
+        }
+  })
+  .then(result => {
+        alert("Bravo "+dataForm.get("prenom")+", vous êtes maintenant inscrit, vous pouvez vous connecter !");
+        document.location.href="/signin";
+  })
+
+  .catch((error) => console.error(error));
 }
