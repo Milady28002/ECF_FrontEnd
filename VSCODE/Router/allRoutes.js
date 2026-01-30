128189ba76c1dbf4a05dbf02680d0c1bcc4d1b02
Nom d'utilisateur GitHubMilady28002 <sylvie.gfm@gmail.com>
2026-01-30 16:11:43 +0100

    Ajout des pages CGV et mentions légales

--- VSCODE/Router/allRoutes.js
+++ VSCODE/Router/allRoutes.js
@@ -12,6 +12,8 @@ export const allRoutes = [
   new Route("/modifPassword","Mot de passe oublié","./pages/auth/modifPassword.html",["client", "admin", "employe"]),
   new Route("/classique", "Classique","./pages/nos-menus/classique.html",[]),
   new Route("/commander", "Commander","./pages/order/commander.html",["client"]),
+  new Route("/cgv", "Conditions generales", "./pages/footer/cgv.html",[]),
+  new Route("/mentions-legales", "Mentions légales", "./pages/footer/mentions-legales.html",[]),
   ];
 
 export const websiteName = "Vite & Gourmand";
