3b1f3e7a4cd0d2312e7983dafe16eecce48cd82b
Milady <sylvie.gfm@gmail.com>
2026-05-06 11:41:42 +0200

    Fix script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://ecfbackendapi-production.up.railway.app";
 const tokenCookieName = "accesstoken";
 const roleCookieName = "role";
 const signoutBtn = document.getElementById("signout-btn");
